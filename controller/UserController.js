e49312620ab6292104bdc45fcbf6bca1ca49bbc5
Reinecke_Fox <72546226+ReynekeJluc@users.noreply.github.com>
2024-10-09 11:00:33 +0800

    Update UserController.js

--- controller/UserController.js
+++ controller/UserController.js
@@ -35,7 +35,7 @@ export const register = async (req, res) => {
 		});
 	} catch (error) {
 		console.log(error);
-		res.status(500).json({
+		res.status(400).json({
 			error: 'Не удалось зарегистрироваться',
 		});
 	}
