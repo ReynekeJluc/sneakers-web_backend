a3d750ff8d3ee36fd80eb7022a818821b62f8c6e
Reinecke_Fox <72546226+ReynekeJluc@users.noreply.github.com>
2024-10-09 11:25:43 +0800

    Update handleValidationErrors.js

--- utils/handleValidationErrors.js
+++ utils/handleValidationErrors.js
@@ -1,7 +1,7 @@
 import { validationResult } from 'express-validator';
 
 export default (req, res, next) => {
-	const errors = validationResult(req);
+	const errors = validationResult(req.msg);
 
 	if (!errors.isEmpty()) {
 		return res.status(400).json(errors.array());
