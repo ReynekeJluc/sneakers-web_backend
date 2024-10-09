1ce206edec9ee1c4dd74b8589a0bb7fa156c0944
Reinecke_Fox <72546226+ReynekeJluc@users.noreply.github.com>
2024-10-09 12:01:54 +0800

    Update post.js

--- validation/post.js
+++ validation/post.js
@@ -12,7 +12,11 @@ export const postCreateValidation = [
 			min: 10,
 		})
 		.isString(),
-	body('price', 'Некорректная цена').isNumeric(),
+	body('price', 'Некорректная цена').isNumeric().custom((value) => 
+		{
+			if (value < 0) return Promise.reject("Price should be positive");
+			else return true;
+		),
 	body('sources', 'Некорректные источники (нужен массив)').optional().isArray(),
 	body('imageUrl', 'Неверная ссылка на картинку').optional(),
 ];
