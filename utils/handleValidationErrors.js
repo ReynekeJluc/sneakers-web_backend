1886a726520fa1cf4cc83c9105f62d5e1f1aaac1
Reinecke_Fox <72546226+ReynekeJluc@users.noreply.github.com>
2024-10-09 11:42:54 +0800

    Update handleValidationErrors.js

--- utils/handleValidationErrors.js
+++ utils/handleValidationErrors.js
@@ -4,8 +4,8 @@ export default (req, res, next) => {
 	const errors = validationResult(req);
 
 	if (!errors.isEmpty()) {
-		return res.status(400).json(errors.array());
 		console.log(errors);
+		return res.status(400).json(errors.array());
 	}
 
 	next();
